9b827b15060a8c871b24326ed069d2ccb5e0d404
Landon Dyken <ldyken53@uab.edu>
2022-01-07 14:18:55 -0600

    Add more option for ideal length

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -156,7 +156,7 @@ class Sidebar extends React.Component<SidebarProps, SidebarState> {
           <Collapsible trigger="Force Directed Options">
             <Form.Label> Ideal Length and Cooling Factor </Form.Label>
             <br/>
-            <input type="range" defaultValue={0.01} min={0.001} max={0.05} step={0.001} onChange={(e) => this.props.setIdealLength(parseFloat(e.target.value))} />
+            <input type="range" defaultValue={0.01} min={0.001} max={0.1} step={0.001} onChange={(e) => this.props.setIdealLength(parseFloat(e.target.value))} />
             <input type="range" defaultValue={0.99} min={0.75} max={0.999} step={0.001} onChange={(e) => this.props.setCoolingFactor(parseFloat(e.target.value))} />
           </Collapsible>
           <Button onClick={(e) => this.props.onSave()}>
