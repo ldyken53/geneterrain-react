b0cd1ba653ebaa5282c9a908fc5166624682949a
Landon Dyken <ldyken53@uab.edu>
2022-03-09 01:02:00 -0600

    Adjust view for graphs

--- shaders/node_frag.wgsl
+++ shaders/node_frag.wgsl
@@ -4,7 +4,7 @@ fn sigmoid(x: f32) -> f32 {
 
 @stage(fragment)
 fn main(@location(0) position: vec2<f32>, @location(1) @interpolate(flat) center: vec2<f32>) -> @location(0) vec4<f32> {
-    if (distance(position, center) > 0.001) {
+    if (distance(position, center) > 0.002) {
         discard;
     }
     return vec4<f32>(0.0, 0.0, 0.0, 1.0 - distance(position, center) * 500.0);
--- src/webgpu/wgsl.tsx
+++ src/webgpu/wgsl.tsx
@@ -303,7 +303,7 @@ export const  node_frag = `fn sigmoid(x: f32) -> f32 {
 
 @stage(fragment)
 fn main(@location(0) position: vec2<f32>, @location(1) @interpolate(flat) center: vec2<f32>) -> @location(0) vec4<f32> {
-    if (distance(position, center) > 0.001) {
+    if (distance(position, center) > 0.002) {
         discard;
     }
     return vec4<f32>(0.0, 0.0, 0.0, 1.0 - distance(position, center) * 500.0);
@@ -354,7 +354,7 @@ fn main(@builtin(instance_index) index : u32, @location(0) position: vec2<f32>)-
 }`;
 export const  edge_frag = `@stage(fragment)
 fn main()->@location(0) vec4<f32>{
-    return vec4<f32>(0.0, 0.0, 0.0, 0.015);
+    return vec4<f32>(0.0, 0.0, 0.0, 0.03);
 }`;
 export const  compute_forces = `struct Node {
     value : f32;
